e1544b839e4e6a996fedc933e9c52d65c28916f6
JakeLopez <SylviaV@Sylvias-MacBook-Pro.local>
2017-08-01 18:35:48 -0500

    “Devour btn not working”

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+//connection to burgers_db database
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+})
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id: " + connection.threadid);
+});
+
+module.exports = connection;
