b9c4ddba8ae452ac7cb27b2c9d5602f2396b8174
Cody Childers <cody_k_childers@homedepot.com>
2017-08-01 19:40:10 -0500

    add sqls

---
+++ seeds.sql
@@ -0,0 +1,5 @@
+use burgers_db;
+
+insert into burgers (burger_name, devoured, date) values ('bacon burger', 0, CURDATE());
+insert into burgers (burger_name, devoured, date) values ('double cheese', 0, CURDATE());
+insert into burgers (burger_name, devoured, date) values ('jalapeno burger', 0, CURDATE());
