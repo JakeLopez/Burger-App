262bc7caa3583d1adb163c03cc3df0721cdcfc68
Cody Childers <cody_k_childers@homedepot.com>
2017-08-01 19:47:05 -0500

    other typo

--- controllers/routes.js
+++ controllers/routes.js
@@ -4,19 +4,26 @@ var burger = require('../models/burger.js');
 
 router.get('/', function(req, res) {
     burger.all(function(burger_data) {
+        console.log("\nThe data:");
         console.log(burger_data);
         res.render('index', { burger_data });
-    })
-})
+    });
+});
 
 router.put('/burgers/update', function(req, res) {
     burger.update(req.body.burger_id, function(result) {
         console.log(result);
         res.redirect('/');
     });
-
-    console.log("something happened");
 });
+
+router.get('/zebra', function(req, res) {
+    console.log('zebra...');
+    res.redirect('/');
+});
+
+//     console.log("something happened");
+// });
 
 
 
--- models/burger.js
+++ models/burger.js
@@ -4,7 +4,11 @@ var burger = {
     all: function(cb) {
         orm.all('burgers', function(res) {
             cb(res);
-        })
+        });
     }
+
+    // TODO:
+    //   make the update function
 }
+
 module.exports = burger;
